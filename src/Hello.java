34a2570b559893b1d3cd0da7959077abf8ec3699
dd929202 <zhangdongdong929@163.com>
2019-09-04 11:43:43 +0800

    1111

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("HELLO WORLD");
+        System.out.print("HELLO\nWORLD"); System.out.print("HELLO WORLD");
+
+        //拍电影
+        // 有剧本今天踹
+        //我说：“今天天气不偶”；
+        //我说：“好无聊啊”；
+    }
+}
+
